2993b055656add55e41061717f1a1de522613e16
Jonathan Poulter <poulter7@gmail.com>
2024-07-26 07:55:28 -0500

    Latest

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -5,4 +5,12 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+
+  imports = [
+  #   ./settings/system.nix
+    # ./settings/environment.nix
+  #   ./settings/yabai.nix
+  #   ./settings/skhd.nix
+  #   ./settings/spacebar.nix
+  ];
 }
