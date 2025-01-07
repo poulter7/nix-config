75a80276b6538e4def14360338b52bf1bef3c88b
Jonathan Poulter <poulter7@gmail.com>
2025-01-07 11:30:15 -0600

    default nvim app to trigger autostarts from wez

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -52,6 +52,7 @@
       export TERM=xterm-256color
       export LANG=en_US.UTF-8
       export POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+      export NVIM_APPNAME=nvim-nixos
       
       # gitgone
       git config --global alias.gone "! git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" {print \$1}' | xargs -r git branch -D"
