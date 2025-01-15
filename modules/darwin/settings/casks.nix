eae9abcb53370344ec899d83367880732f09770b
Jonathan Poulter <poulter7@gmail.com>
2025-01-15 11:56:20 -0600

    tofns

--- modules/darwin/settings/casks.nix
+++ modules/darwin/settings/casks.nix
@@ -1,46 +1,12 @@
 { lib, ... }: {
-  imports = [
-    ../../userpkgs.nix
-  ];
   homebrew = {
+    brews = import ../../userpkgs.nix homebrew-brews;
+    casks = import ../../userpkgs.nix homebrew-casks;
     enable = true;
     onActivation = {
       autoUpdate = true;
       cleanup = "uninstall";
       upgrade = true;
     };
-    casks = [
-      "docker"
-      "spotify"
-      "postman"
-      "zoom"
-      "whatsapp"
-      "amethyst"
-      "dash"
-      "texshop"
-      "trader-workstation"
-      "ibkr"
-      "raycast"
-      "visual-studio-code"
-      "skim"
-      "bruno"
-      "google-chrome"
-      "todoist"
-      "karabiner-elements"
-      "steam"
-      "sublime-text"
-      "logi-options+"
-      "readdle-spark"
-      "cursor"
-      "jetbrains-toolbox"
-      "chatgpt"
-      "qgis"
-      "active-trader-pro"
-      "caffeine"
-      "font-hack-nerd-font"
-      "virtual-desktop-streamer"
-      "wezterm"
-      "ollama"
-    ];
   };
 }
--- modules/userpkgs.nix
+++ modules/userpkgs.nix
@@ -1,12 +1,43 @@
-{
-  homebrew {
-    brews = [
+homebrew-brews: 
+    [
         "graphviz"
         "swiftlint"
         "entr"
         "zoxide"
         "zig"
         "awscli"
+        "fish"
     ];
-  }
-}
+homebrew-casks: [
+    "docker"
+    "spotify"
+    "postman"
+    "zoom"
+    "whatsapp"
+    "amethyst"
+    "dash"
+    "texshop"
+    "trader-workstation"
+    "ibkr"
+    "raycast"
+    "visual-studio-code"
+    "skim"
+    "bruno"
+    "google-chrome"
+    "todoist"
+    "karabiner-elements"
+    "steam"
+    "sublime-text"
+    "logi-options+"
+    "readdle-spark"
+    "cursor"
+    "jetbrains-toolbox"
+    "chatgpt"
+    "qgis"
+    "active-trader-pro"
+    "caffeine"
+    "font-hack-nerd-font"
+    "virtual-desktop-streamer"
+    "wezterm"
+    "ollama"
+];
