8808c3e235de77a7221ac45443377e830e49ecd3
Jonathan Poulter <poulter7@gmail.com>
2025-01-31 10:24:05 -0600

    move kickstart into main repo

---
+++ modules/home-manager/kickstart.nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,366 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+local function list_insert_unique(dst, src)
+  if not dst then
+    dst = {}
+  end
+  local added = {}
+  for _, val in ipairs(dst) do
+    added[val] = true
+  end
+  for _, val in ipairs(src) do
+    if not added[val] then
+      table.insert(dst, val)
+      added[val] = true
+    end
+  end
+  return dst
+end
+
+local function get_folder_if_exists(path)
+  return vim.uv.fs_stat(path) and path or nil
+end
+
+return {
+  {
+    'mrjones2014/smart-splits.nvim',
+  },
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+    config = true,
+  },
+  {
+    'stevearc/resession.nvim',
+    opts = {},
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    commit = '193786e0371e3286d3bc9aa0079da1cd41beaa62',
+    opts = function(opts)
+      opts.direction = 'float'
+      return opts
+    end,
+  },
+  {
+    'tadaa/vimade',
+    -- default opts (you can partially set these or configure them however you like)
+    opts = {
+      fadelevel = 0.7, -- any value between 0 and 1. 0 is hidden and 1 is opaque.
+    },
+  },
+  {
+    'anuvyklack/windows.nvim',
+    dependencies = {
+      'anuvyklack/middleclass',
+      'anuvyklack/animation.nvim',
+    },
+    opts = {
+      ignore = {
+        buftype = { 'quickfix', 'nofile' }, -- nofile is for neotest's main buffer
+        filetype = { 'NvimTree', 'neo-tree', 'undotree', 'gundo' },
+      },
+    },
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    config = function()
+      --set colorscheme
+      require('kanagawa').setup {
+        overrides = function() -- add/modify highlights
+          return {
+            BufferCurrent = { bg = '#e98a00', fg = '#000000' },
+            BufferCurrentMod = { link = 'BufferCurrent' },
+          }
+        end,
+      }
+    end,
+  },
+  -- co-pilot will rot your brain
+  -- {
+  --   'zbirenbaum/copilot.lua',
+  --   opts = {
+  --     suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<C-Enter>' } },
+  --     panel = { enabled = false },
+  --   },
+  -- },
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-python',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-python' {
+            dap = { justMyCode = false },
+          },
+        },
+      }
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        pyright = {
+          enabled = false,
+        },
+        basedpyright = {},
+      },
+    },
+  },
+  {
+    'nvim-zh/colorful-winsep.nvim',
+    config = true,
+    event = { 'BufEnter' },
+    opts = {
+      smooth = false,
+      hi = {
+        fg = '#e98a00',
+      },
+    },
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- TODO: not working OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy', -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      vim.diagnostic.config { virtual_text = false }
+      require('tiny-inline-diagnostic').setup {
+        preset = 'modern',
+        transparent_bg = false,
+        hi = {
+          background = 'None',
+        },
+        options = {
+          -- Show the source of the diagnostic.
+          show_all_diags_on_cursorline = false,
+          virt_texts = {
+            -- Priority for virtual text display
+            priority = 4098,
+          },
+          enable_on_insert = false,
+        },
+      }
+    end,
+  },
+  {
+    'smoka7/hop.nvim',
+    opts = {},
+  },
+  {
+    'rebelot/heirline.nvim',
+    opts = function(_, opts)
+      opts.tabline = nil -- remove tabline
+    end,
+  },
+  {
+    'jsongerber/thanks.nvim',
+    config = true,
+  },
+  { 'sQVe/sort.nvim' },
+  { 'mistweaverco/kulala.nvim', opts = {} },
+  {
+    '0x00-ketsu/autosave.nvim',
+    -- lazy-loading on events
+    event = { 'InsertLeave', 'TextChanged' },
+    config = function()
+      require('autosave').setup {}
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap-python',
+    config = function()
+      local dap_python = require 'dap-python'
+      local adapter_python_path = require('mason-registry').get_package('debugpy'):get_install_path() .. '/venv/bin/python'
+
+      dap_python.setup(adapter_python_path)
+    end,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim', -- use mason-lspconfig to configure LSP installations
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = list_insert_unique(opts.ensure_installed, {
+        'lua_ls',
+        'basedpyright',
+        -- add more arguments for adding more language servers
+      })
+    end,
+  },
+  {
+    'jay-babu/mason-null-ls.nvim', -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+    -- overrides `require("mason-null-ls").setup(...)`
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'williamboman/mason.nvim',
+      'nvimtools/none-ls.nvim',
+    },
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = list_insert_unique(opts.ensure_installed, {
+        'prettier',
+        'stylua',
+        -- add more arguments for adding more null-ls sources
+      })
+    end,
+  },
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = list_insert_unique(opts.ensure_installed, {
+        'python',
+        -- add more arguments for adding more debuggers
+      })
+    end,
+  },
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      'mfussenegger/nvim-dap-python', --optional
+      { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    },
+    lazy = false,
+    branch = 'regexp', -- This is the regexp branch, use this for the new version
+    config = function()
+      require('venv-selector').setup()
+    end,
+  },
+  {
+    'mikavilpas/yazi.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+    },
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ['q'] = 'close_window',
+          },
+        },
+      },
+    },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+  {
+    'poulter7/arrow.nvim',
+    dir = get_folder_if_exists '/Users/jonathan/Code/projects/arrow.nvim/',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons' },
+      -- or if using `mini.icons`
+      -- { "echasnovski/mini.icons" },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = '\t', -- Recommended to be a single key
+      buffer_leader_key = '<S-\t>', -- Per Buffer Mappings
+    },
+  },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+  { -- optional cmp completion source for require statements and module annotations
+    'hrsh7th/nvim-cmp',
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = 'lazydev',
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+      adapters = {
+        hosted = function()
+          return require('codecompanion.adapters').extend('openai_compatible', {
+            env = {
+              url = 'http://127.0.0.1:8080',
+              api_key = 'key',
+              chat_url = '/v1/chat/completions',
+            },
+          })
+        end,
+      },
+      strategies = {
+        -- Change the default chat adapter
+        chat = {
+          adapter = 'hosted',
+        },
+      },
+      opts = {
+        -- Set debug logging
+        log_level = 'DEBUG',
+      },
+    },
+  },
+}
---
+++ modules/home-manager/kickstart.nvim/lua/custom/plugins/polish.lua
@@ -0,0 +1,208 @@
+-- buffer pick
+require('Comment').setup {
+  toggler = {
+    line = '<Leader>cc',
+    block = '<Leader>cb',
+  },
+  opleader = {
+    line = '<Leader>cc',
+    block = '<Leader>cb',
+  },
+}
+
+--- keybindings
+local tele = require 'telescope.builtin'
+local nx = { 'n', 'x' }
+local nv = { 'n', 'v' }
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd [[cab cc CodeCompanion]]
+require('which-key').add {
+  --
+  { '<C-h>', require('smart-splits').move_cursor_left },
+  { '<C-j>', require('smart-splits').move_cursor_down },
+  { '<C-k>', require('smart-splits').move_cursor_up },
+  { '<C-l>', require('smart-splits').move_cursor_right },
+  { '-', '<Cmd>split<CR>' },
+  { '|', '<Cmd>vsplit<CR>' },
+  { '<C-w>=', '<Cmd>WindowsEqualize<CR>' },
+  { '<C-w>+', '<Cmd>WindowsMaximize<CR>' },
+  -- CodeCompanion
+  { '<C-a>', '<Cmd>CodeCompanionActions<CR>' },
+  { '<Leader>a', '<Cmd>CodeCompanionChat Toggle<CR>' },
+  { 'ga', '<Cmd>CodeCompanionChat<CR>' },
+  --- toggle term
+  { '\\', '<Cmd>execute v:count . "ToggleTerm"<CR>' },
+  { '\\', '<Cmd>ToggleTerm<CR>', mode = 't' },
+  -- code
+  { '<leader>c', group = '[C]ode', mode = nx },
+  { '<leader>a', group = '[A]rrow', mode = nx },
+  { '<Leader>cv', '<cmd>VenvSelect<cr>' },
+  { '<leader>u', group = '[U]ser Interface' },
+  { '<leader>ut', '<Cmd>Neotest summary<CR>', desc = 'UI: toggle neotest summary' },
+  { '<leader>b', group = '[B]uffers' },
+  { '<leader>bp', '<Cmd>BufferPick<CR>', desc = '[B]uffer [P]ick' },
+  { '<leader>bx', '<Cmd>BufferPickDelete<CR>', desc = '[B]uffer to [x]' },
+  { '<leader>f', group = '[F]ind' },
+  { '<leader>fe', '<Cmd>Neotree toggle<CR>', desc = '[F]ind t[E]lescope', silent = true },
+  { '<leader>fh', tele.help_tags, desc = '[F]ind [H]elp' },
+  { '<leader>fk', tele.keymaps, desc = '[F]ind [K]eymaps' },
+  { '<leader>ff', tele.find_files, desc = '[F]ind [F]iles' },
+  { '<leader>ft', tele.builtin, desc = '[F]ind a [T]elescope' },
+  { '<leader>fw', tele.live_grep, desc = '[F]ind by [\\w]ord (Grep)' },
+  { '<leader>fd', tele.diagnostics, desc = '[F]ind [D]iagnostics' },
+  {
+    '<leader>fo',
+    function()
+      tele.oldfiles { only_cwd = true }
+    end,
+    desc = '[F]ind [O]ld files',
+  },
+  {
+    '<leader>fO',
+    function()
+      tele.oldfiles { only_cwd = false }
+    end,
+    desc = '[F]ind [O]ld files from everywhere',
+  },
+  { '<leader>f.', tele.resume, desc = '[F]ind Recent Files ("." for repeat)' },
+  {
+    '<leader>/',
+    function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      tele.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end,
+    desc = '[/] Fuzzily search in current buffer',
+  },
+  -- Open in the current file's parent
+  {
+    '<leader>f-',
+    '<cmd>Yazi<cr>',
+    mode = nv,
+    desc = '[F]ind using [Y]azi',
+  },
+  {
+    '<leader>f_', -- Open in the current working directory
+    '<cmd>Yazi cwd<cr>',
+    desc = '[F]ind using [Y]azi in the cwd',
+  },
+  -- Debug
+  { '<leader>d', group = '[D]ebug' },
+  { '<leader>dm', '<Cmd>lua require("neotest").run.run { strategy = "dap" }<CR>', desc = '[D]ebug Test [M]ethod' },
+  { '<leader>db', '<Cmd>lua require("dap").toggle_breakpoint()<CR>', desc = '[D]ebug: [B]reakpoint' },
+  { '<leader>dc', '<Cmd>lua require("dap").continue()<CR>', desc = '[D]ebug: [C]ontinue' },
+  { '<leader>dd', '<Cmd>lua require("dap").step_into()<CR>', desc = '[D]ebug: Step Into' },
+  { '<leader>do', '<Cmd>lua require("dap").step_over()<CR>', desc = '[D]ebug: Step Over' },
+  { '<leader>dr', '<Cmd>lua require("dap").step_out()<CR>', desc = '[D]ebug: Step Out' },
+  { '<leader>du', '<Cmd>lua require("dapui").toggle()<CR>', desc = '[D]ebug: See last session result.' },
+  { '<leader>dv', '<Cmd>lua require("dap-view").toggle()<CR>', desc = 'dap-view toggle' },
+  { '<Bs>', '<Cmd>DapToggleRepl<CR>', desc = '[D]ebug: repl' },
+  { '<leader>ua', '<Cmd>lua _aider_toggle()<CR>', desc = 'ToggleTerm aider' },
+  -- Hop
+  {
+    'f',
+    function()
+      require('hop').hint_words { multi_windows = true }
+    end,
+    mode = { 'n' },
+    desc = 'Hop hint words',
+  },
+  {
+    '<S-f>',
+    function()
+      require('hop').hint_lines { multi_windows = true }
+    end,
+    mode = { 'n' },
+    desc = 'Hop hint lines',
+  },
+  {
+    'f',
+    function()
+      require('hop').hint_words { extend_visual = true }
+    end,
+    mode = { 'v' },
+    desc = 'Hop hint words',
+  },
+  {
+    '<S-f>',
+    function()
+      require('hop').hint_lines { extend_visual = true }
+    end,
+    mode = { 'v' },
+    desc = 'Hop hint lines',
+  },
+}
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new {
+  cmd = 'lazygit',
+  dir = 'git_dir',
+  direction = 'float',
+  float_opts = {
+    border = 'double',
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd 'startinsert!'
+    vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+  end,
+  -- function to run on closing the terminal
+  on_close = function()
+    vim.cmd 'startinsert!'
+  end,
+}
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+--Post-init
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    vim.cmd.colorscheme 'kanagawa'
+    -- vim.cmd 'VimadeFocus' -- this breaks ToggleTerm form now
+  end,
+})
+
+-- set resession to work within a directory
+local resession = require 'resession'
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    -- Only load the session if nvim was started with no args and without reading from stdin
+    if vim.fn.argc(-1) == 0 and not vim.g.using_stdin then
+      -- Save these to a different directory, so our manual sessions don't get polluted
+      resession.load(vim.fn.getcwd(), { dir = 'dirsession', silence_errors = true })
+    end
+  end,
+  nested = true,
+})
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  callback = function()
+    resession.save(vim.fn.getcwd(), { dir = 'dirsession', notify = false })
+  end,
+})
+vim.api.nvim_create_autocmd('StdinReadPre', {
+  callback = function()
+    -- Store this for later
+    vim.g.using_stdin = true
+  end,
+})
+
+-- Arrow handlers to get buffer -> line -> hop functionality
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'ArrowOpenFile',
+  callback = function(e)
+    vim.schedule(function()
+      local bfr = vim.api.nvim_get_current_buf()
+      local bookmarks = require('arrow.buffer_persist').get_bookmarks_by(bfr)
+      if #bookmarks > 0 then
+        require('arrow.buffer_ui').openMenu(bfr)
+      end
+    end)
+  end,
+})
+-- return plugins (this is a polish file, so we don't need to return anything)
+return {}
