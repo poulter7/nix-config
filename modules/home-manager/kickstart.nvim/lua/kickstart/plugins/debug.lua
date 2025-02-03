587bd0577e2d29d3ba5a9f61ac1444c532250525
Jonathan Poulter <poulter7@gmail.com>
2025-02-03 14:33:55 -0600

    Remove go dap configuration

--- modules/home-manager/kickstart.nvim/lua/kickstart/plugins/debug.lua
+++ modules/home-manager/kickstart.nvim/lua/kickstart/plugins/debug.lua
@@ -110,14 +110,6 @@ return {
     -- dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     -- dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
     require('nvim-dap-virtual-text').setup {
       commented = true,
     }
