f52e74e717cff6233180e05a93eb971675617aa2
Jonathan Poulter <poulter7@gmail.com>
2025-02-12 11:43:46 -0600

    linux-jonathan map

---
+++ flakes/linux-jonathan/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix OS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-23.11
+    nixGL.url = "github:nix-community/nixGL";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+  };
+
+outputs = inputs@{ nixpkgs, home-manager, nixvim, ... }: 
+    let
+      user = "jonathan";
+      system = "aarch64-linux";
+	userroot = "/home";
+    in {
+homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+	pkgs = import nixpkgs { system = system;config.allowUnfree = true; };
+	modules = [
+nixvim.homeManagerModules.nixvim
+(import ../../modules/home-manager {user=user; userroot=userroot; })
+{
+home.username = user;
+home.homeDirectory = "${userroot}/${user}";
+home.stateVersion = "24.11";
+}
+	];
+  };
+};
+}
+
