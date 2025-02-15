3cc1125e3ba57a31a82e692a731bb968fb30a695
Jonathan Poulter <poulter7@gmail.com>
2025-02-15 13:44:56 -0600

    remove commented line

--- modules/home-manager/kickstart.nvim/init.lua
+++ modules/home-manager/kickstart.nvim/init.lua
@@ -661,7 +661,6 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
         python = { 'ruff_format' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
